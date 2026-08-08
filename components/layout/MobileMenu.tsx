a51c9c67f40c527caabdcbe1e063f1b9dd7d75c9
Keysos <gustavoxabreux@gmail.com>
2026-08-08 11:28:08 -0300

    style: modify navbar design

---
+++ components/layout/MobileMenu.tsx
@@ -0,0 +1,51 @@
+import { Button } from "@/components/ui/button";
+import { Menu } from "lucide-react";
+
+import Link from "next/link";
+
+type MobileMenuProps = {
+  className: string;
+  isOpen: boolean;
+  handleClick: () => void;
+};
+
+export default function MobileMenu({
+  className,
+  handleClick,
+  isOpen,
+  ...props
+}: MobileMenuProps) {
+  return (
+    <div className="flex flex-col">
+      <Button
+        variant={"ghost"}
+        className={className}
+        onClick={handleClick}
+        {...props}
+      >
+        <Menu
+          className={`h-5 w-5 transition-transform duration-300 ${isOpen ? "-rotate-90" : "rotate-0"}`}
+        />
+      </Button>
+
+      <div
+        className={`flexflex-col absolute top-full left-0 z-50 w-full transition-all duration-300 sm:hidden ${isOpen ? "translate-y-0 opacity-100" : "pointer-events-none -translate-y-2 opacity-0"}`}
+      >
+        <div className="bg-background flex flex-col gap-2 border-b-2 p-2">
+          <Link
+            className="bg-primary text-primary-foreground hover:bg-primary/80 rounded-4xl p-2 text-center"
+            href="/login"
+          >
+            Log In
+          </Link>
+          <Link
+            className="bg-primary text-primary-foreground hover:bg-primary/80 rounded-4xl p-2 text-center"
+            href="/signup"
+          >
+            Sign Up
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -1,18 +1,44 @@
+"use client";
+
 import Link from "next/link";
-import NavbarMenu from "./NavbarMenu";
+import MobileMenu from "./MobileMenu";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  function handleClick() {
+    setIsOpen((prev) => !isOpen);
+  }
+
   return (
-    <div className="flex justify-end py-2 px-2 w-full">
-      <nav className="flex items-center gap-4">
-        <Link href="/games">Games</Link>
-        <Link className="hidden sm:flex" href="/login">
+    <div className="bg-background relative">
+      <nav
+        className={`${isOpen ? "" : "border-border border-b-2"} flex w-full items-center justify-end gap-4 p-2 sm:p-3`}
+      >
+        <Link
+          href="/games"
+          className="text-muted-foreground hover:text-foreground transition-colors duration-300"
+        >
+          Games
+        </Link>
+        <Link
+          className="text-muted-foreground hover:text-foreground hidden transition-colors duration-300 sm:flex"
+          href="/login"
+        >
           Log In
         </Link>
-        <Link className="hidden sm:flex" href="/signup">
-          Register
+        <Link
+          className="text-muted-foreground hover:text-foreground hidden transition-colors duration-300 sm:flex"
+          href="/signup"
+        >
+          Sign Up
         </Link>
-        <NavbarMenu />
+        <MobileMenu
+          handleClick={handleClick}
+          isOpen={isOpen}
+          className="text-muted-foreground hover:bg-accent hover:text-foreground cursor-pointer sm:hidden"
+        />
       </nav>
     </div>
   );
