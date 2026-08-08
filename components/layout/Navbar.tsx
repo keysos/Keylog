1ab421dc6e347ba343e96f878783bd6dbb3c6c15
Keysos <gustavoxabreux@gmail.com>
2026-08-08 01:16:26 -0300

    feat: create navbar

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -4,8 +4,14 @@ import NavbarMenu from "./NavbarMenu";
 const Navbar = () => {
   return (
     <div className="flex justify-end py-2 px-2 w-full">
-      <nav className="flex items-center gap-2">
+      <nav className="flex items-center gap-4">
         <Link href="/games">Games</Link>
+        <Link className="hidden sm:flex" href="/login">
+          Log In
+        </Link>
+        <Link className="hidden sm:flex" href="/signup">
+          Register
+        </Link>
         <NavbarMenu />
       </nav>
     </div>
